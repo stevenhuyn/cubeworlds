e0e7d359676d0987d37b3453c422eb0a810e631d
stevenhuyn <stevenhuyn@gmail.com>
2023-05-01 15:19:46 -0700

    Change background color

--- cubeway/src/lib.rs
+++ cubeway/src/lib.rs
@@ -500,9 +500,9 @@ impl State {
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
+                            r: 0.0,
+                            g: 0.133,
+                            b: 0.251,
                             a: 1.0,
                         }),
                         store: true,
