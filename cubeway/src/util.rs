cecd2818ee8c294d6cb532c85bedd70bd269706f
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2023-11-15 19:56:42 -0800

    Use cooler random function

--- cubeway/src/util.rs
+++ cubeway/src/util.rs
@@ -3,7 +3,7 @@ pub fn rand(seed: u32) -> f32 {
     let hash = (seed.wrapping_mul(0x5D588B68))
         .wrapping_add(0x269EC3)
         .rotate_left(17)
-        .wrapping_mul(0x5D588B);
+        .wrapping_mul(0xADD42B);
 
     (hash % 1_000_000_000) as f32 / 1_000_000_000 as f32
 }
